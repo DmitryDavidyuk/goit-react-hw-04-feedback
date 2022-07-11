a50629c9461abe7326dc47badfc0c0c99388ea89
Dmitry Davidyk <91990771+DmitryDavidyuk@users.noreply.github.com>
2022-07-12 00:44:41 +0300

    task 2
    add fi;e and created сlass feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+
+class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  hendleIncrement = event => {
+    if (event.target.nodeName !== 'BUTTON') {
+      return;
+    }
+    const name = event.target.name;
+    console.log(name);
+    this.setState(prevState => ({ [name]: prevState[name] + 1 }));
+  };
+
+  render() {
+    return (
+      <div onClick={this.hendleIncrement}>
+        <h2>Please leave feedback</h2>
+        <button name="good">Good</button>
+        <button name="neutral">Neutral</button>
+        <button name="bad">Bad</button>
+        <h2>Statistics</h2>
+        <ul>
+          <li>Good: {this.state.good}</li>
+          <li>Neutral: {this.state.neutral}</li>
+          <li>Bad: {this.state.bad}</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
